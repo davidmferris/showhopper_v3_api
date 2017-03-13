c640237178208cc7980219f0b5799a529b2ecdf4
Dave Ferris <davidmferris@gmail.com>
2017-03-12 21:16:13 -0700

    set up rubocop

--- app/controllers/api/v1/shows_controller.rb
+++ app/controllers/api/v1/shows_controller.rb
@@ -1,7 +1,9 @@
-module Api::V1
-  class ShowsController < ApiController
-    api! 'A list of shows'
-    def index
+module Api
+  module V1
+    class ShowsController < ApiController
+      api! 'A list of shows'
+      def index
+      end
     end
   end
 end
